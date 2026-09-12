73c52cd8e30340583e6feae49ac62a653b5a209e
ChEnLeo-7 <ChEnLeo-7@users.noreply.github.com>
2026-09-12 15:40:03 +0000

    test(downloads): support OkHttp call tags

--- app/src/test/kotlin/com/wallhub/android/data/downloads/SteamCdnHttpsTest.kt
+++ app/src/test/kotlin/com/wallhub/android/data/downloads/SteamCdnHttpsTest.kt
@@ -25,6 +25,7 @@ import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Assert.assertThrows
 import org.junit.Test
+import kotlin.reflect.KClass
 
 class SteamCdnHttpsTest {
     @Test
@@ -307,6 +308,14 @@ private class ObservableCall(
     override fun isCanceled(): Boolean = cancelled
 
     override fun timeout(): Timeout = Timeout.NONE
+
+    override fun <T : Any> tag(type: KClass<T>): T? = null
+
+    override fun <T> tag(type: Class<out T>): T? = null
+
+    override fun <T : Any> tag(type: KClass<T>, computeIfAbsent: () -> T): T = computeIfAbsent()
+
+    override fun <T : Any> tag(type: Class<T>, computeIfAbsent: () -> T): T = computeIfAbsent()
 
     override fun clone(): Call = ObservableCall(body)
 }
